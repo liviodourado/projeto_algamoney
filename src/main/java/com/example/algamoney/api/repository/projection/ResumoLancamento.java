0c235a2df4c2564f9db1ebab046600fa72770393
Livio Dourado <liviodourado@gmail.com>
2020-07-28 16:27:59 -0300

    Criando Projeção (Resumo) de Lançamentos

---
+++ src/main/java/com/example/algamoney/api/repository/projection/ResumoLancamento.java
@@ -0,0 +1,95 @@
+package com.example.algamoney.api.repository.projection;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.example.algamoney.api.model.TipoLancamento;
+
+public class ResumoLancamento {
+
+	private Long codigo;
+	private String descricao;
+	private LocalDate dataVencimento;
+	private LocalDate dataPagamento;
+	private BigDecimal valor;
+	private TipoLancamento tipo;
+	private String nome;
+	private String categoria;
+
+	public ResumoLancamento(Long codigo, String descricao, LocalDate dataVencimento, LocalDate dataPagamento,
+			BigDecimal valor, TipoLancamento tipo, String nome, String categoria) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+		this.valor = valor;
+		this.tipo = tipo;
+		this.nome = nome;
+		this.categoria = categoria;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public LocalDate getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(LocalDate dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public TipoLancamento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoLancamento tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+}
--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,11 @@ public class LancamentoService {
 		}
 		
 		return lancamentoRepository.save(lancamento);
+	}
+	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Optional<Lancamento> lancamentoSalvo = lancamentoRepository.findById(codigo);
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		return salvar(lancamento);
 	}
 }
