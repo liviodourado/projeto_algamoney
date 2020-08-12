2332d4330c3c98bff06af9952e18a0a6dca7aa15
Livio Dourado <liviodourado@gmail.com>
2020-08-11 21:06:16 -0300

    Criando os profiles Basic e OAuth2

--- src/main/java/com/example/algamoney/api/config/OAuthSecurityConfig.java
+++ src/main/java/com/example/algamoney/api/config/OAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@Profile("oauth-security")
 @EnableWebSecurity
 public class OAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
--- src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,9 +24,9 @@ public class AppUserDetailsService implements UserDetailsService{
 	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		Optional<Usuario> usuarioOprional = usuarioRepository.findByEmail(email);
-		Usuario usuario = usuarioOprional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou Senha incorretos"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou Senha incorretos"));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
